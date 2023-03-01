08a0fa9dcb996b85449f65a9eda254f7ebe4ef5a
Cenk k <cenksforcs@gmail.com>
2023-03-01 19:10:21 +0100

    just a real simple gui aka buttons (using dom manipulation)

--- jscript.js
+++ jscript.js
@@ -1,30 +1,68 @@
-
+let playerResult=0;
+let computerResult=0;
+let result="";
 
 function getComputerChoice(){
     
-let array= ["ROCK" , "PAPER", "SCISSOR"];
+let array= ["rock" , "paper", "scissor"];
     return array[Math.floor(Math.random()*3)];
 }
 
+
+
+const div = document.querySelectorAll('div');
+div.forEach(div => {
+    div.style.margin='20px';
+    div.style.display='flex';
+    div.style.justifyContent='center';
+    div.style.alignContent='center';    
+   
+});
+
+
 // burada computerSelection i da otomatik yollayan yok yalniz game() disinda! game()harici
 //kullacaksan bi randomu calistirt iste getComputerChoice()u yani
+
 function playRound(playerSelection, computerSelection){
-   let pSelection= playerSelection.toUpperCase();
+   let pSelection= playerSelection.toLowerCase();
+   
+ 
 
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+    
+        playerResult += 1
+    result = `You win! ${playerSelection} beats ${computerSelection}
+        Player score: ${playerResult} Computer score: ${computerResult}`
 
-   if (pSelection===computerSelection) return "It's a tie!";
-   if (pSelection==="ROCK"){
-    return (computerSelection==="PAPER")? "You lose! Paper beats Rock" : "You win Rock beats Scissor";
-   }
-   if(pSelection==="PAPER"){
-    return (computerSelection==="SCISSOR")? "You lose! Scissor beats Paper" : "You win! Paper beats Rock";
-   }
-   if(pSelection==="SCISSOR"){
-    return (computerSelection==="ROCK")? "You lose! Rock beats Scissor!" : "You win! Scissor beats Paper";
-   }
+    if (playerResult == 5) {
+        result += 'You won the game!'
+        playerResult=0;
+        computerResult=0;
+    }
+}
+else if (playerSelection == computerSelection) {
+    result = ('It\'s a tie. You both chose ' + playerSelection
+        + "\n\nPlayer score: " + playerResult + "\nComputer score: " + computerResult)
+}
+else {
+    computerResult += 1
+    result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+        + "\nPlayer score: " + playerResult + "\nComputer score: " + computerResult)
+
+    if (computerResult == 5) {
+        result += ' I won the game! '
+        playerResult=0;
+        computerResult=0;
+   
+    }
+}
+document.getElementById('score').textContent=result;
+return
 }
 
- function game(playerSelection){
+ /* function game(playerSelection){
     let playerWin=0;
     let computerWin=0;
 
@@ -37,7 +75,7 @@ function playRound(playerSelection, computerSelection){
     if (playerWin === computerWin) return " it's a tie! Both of you win " +playerWin +" games and tied " +(5-playerWin) +" games!"; 
     return (playerWin>=3)?  "You are the winner! Out of 5 games you win " +playerWin+" of them!" :  "You lose! Out of 5 games you only win " +playerWin +" of them!" ;
 
-}
+} 
 
 
 
@@ -48,3 +86,16 @@ function playRound(playerSelection, computerSelection){
     }
 
     game(uInput);
+*/
+
+
+const buttons = document.querySelectorAll('button');
+
+
+
+buttons.forEach(button => {
+    button.style.padding='10px';
+    button.style.margin='20px';
+    button.addEventListener('click', ()=> playRound(button.id, getComputerChoice() ) )
+});
+
